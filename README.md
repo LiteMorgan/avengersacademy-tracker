4b5379e727f6cb5ee7c3e60ebbab78ce920ae07a
Liam Morgan <liam.morgan@elviscommunications.com>
2016-05-09 21:47:09 +0100

    v0.1.0 release. Initialized with Gulp Starter

---
+++ README.md
@@ -0,0 +1,4 @@
+# avengersacademy-tracker
+A personal Event Tracker for TinyCo's Avengers Academy, on iOS and Android.
+
+Tracker can be used here: [http://avengers.getignited.co.uk](http://avengers.getignited.co.uk)
---
+++ src/javascripts/app.js
@@ -0,0 +1,243 @@
+const underscore = require('underscore');
+global._ = underscore
+
+
+// getJSON Function
+function get(url) {
+  // Return a new promise.
+  return new Promise(function(resolve, reject) {
+    // Do the usual XHR stuff
+    var req = new XMLHttpRequest();
+    req.open('GET', url);
+
+    req.onload = function() {
+      // This is called even on 404 etc
+      // so check the status
+      if (req.status == 200) {
+        // Resolve the promise with the response text
+        resolve(req.response);
+      }
+      else {
+        // Otherwise reject with the status text
+        // which will hopefully be a meaningful error
+        reject(Error(req.statusText));
+      }
+    };
+
+    // Handle network errors
+    req.onerror = function() {
+      reject(Error("Network Error"));
+    };
+
+    // Make the request
+    req.send();
+  });
+}
+
+function getJSON(url) {
+  return get(url).then(JSON.parse);
+}
+
+getJSON('/global.json').then(function(data){
+  // get JSON data
+  return data;
+}).catch(function(error){
+  $('.error').show();
+}).then(function(data){
+  // Take JSON data.characters and build Character Tracker
+  $.each($(data), function(i, data){
+    $.each($(data.characters), function(i, char){
+      trackerBuild(char, characterTracker);
+    });
+    // Take JSON data.costumes and build Costumes Tracker
+    $.each($(data.costumes), function(i, cost){
+      trackerBuild(cost, costumeTracker);
+    });
+    // Take JSON data.buildings and build Buildings Tracker
+    $.each($(data.buildings), function(i, build){
+      trackerBuild(build, buildingTracker);
+    });
+  });
+  return data;
+}).then(function(data){
+  // Function to take all items
+  // Add to an array
+  // Sort
+  // And then return combined totals
+  itemCards(data, function(){
+    itemTotals();
+  });
+}).then(function(){
+  if (!localStorage['aaCivilWar']) {
+    localStorage['aaCivilWar'] = JSON.stringify({});
+  } else {
+    var stored = JSON.parse(localStorage.aaCivilWar),
+        storedLength = Object.keys(stored).length;
+
+    for (var i = 0; i < storedLength; i++) {
+      var key = Object.keys(stored)[i];
+      if(stored[key].status){
+        var check = $('#trackerApp').find('#' + key);
+        check.prop('checked', true);
+      }
+    }
+  }
+}).then(function(){
+  finishedItems();
+  itemTotals();
+  zeroItems();
+});
+
+// Build Tracker
+function trackerBuild(data, location){
+  $(location).append(trackerHTML(data.name, data.frag, data.mats, data.premium));
+};
+
+function trackerHTML(name, frag, items, premium){
+  var i = 0,
+      line,
+      isPremium = '',
+      itemArray = [],
+      itemList = $.each($(items), function(i, mats){
+                   itemArray += '<li><span class="item__req" data-item="' +
+                                mats.frag +
+                                '">' +
+                                mats.req +
+                                '</span>' +
+                                mats.item +
+                                '</li>';
+                 });
+
+  if(premium === true){ isPremium = ' table--premium'; } 
+
+  line = '<section class="table__row' + isPremium + '">' +
+         '<div class="table__col table__col--xs input__check">' +
+         '<input class="input__check--box" type="checkbox" ' +
+         'name="' + frag + 'Got" id="' + frag + '" />' +
+         '<label class="input__check--label" for="' + frag + '"></label>' +
+         '</div>' +
+         '<div class="table__col table__col--md">' +
+         name +
+         '</div>' +
+         '<div class="table__col table__col--lg" id="itemList">' +
+         '<ul>' +
+         itemArray +
+         '</ul>' +
+         '</div>' +
+         '</section>';
+
+  return line;
+};
+
+function itemCards(data, callback){
+  var allItems = new Array();
+
+  $.each($(data.characters), function(i, info){
+    $.each($(info.mats), function(j, mats){
+      allItems.push({"frag": mats.frag, "name": mats.item});
+    });
+  });
+  $.each($(data.costumes), function(i, info){
+    $.each($(info.mats), function(j, mats){
+      allItems.push({"frag": mats.frag, "name": mats.item});
+    });
+  });
+  $.each($(data.buildings), function(i, info){
+    $.each($(info.mats), function(j, mats){
+      allItems.push({"frag": mats.frag, "name": mats.item});
+    });
+  });
+  allItems = $.makeArray(allItems);
+  allItems = _.uniq(allItems, function(item, key, frag){
+               return item.frag;
+             });
+
+  $.each($(allItems), function(i, item){
+    var itemHTML = '<div class="event__item" data-frag="' +
+                    item.frag +
+                    '">' +
+                    '<div class="event__item--total"></div>' +
+                    item.name +
+                    '</div>';
+
+    $('#eventItems').append(itemHTML);
+  });
+
+  callback();
+};
+
+function itemTotals(){
+  $.each($('.event__item'), function(){
+    var ele = $(this),
+        frag = ele.data('frag'),
+        rows = $('#trackerApp').find('.table__row'),
+        sum = 0;
+
+    rows.each(function(){
+      
+
+      var ele = $(this),
+          rowComplete = ele.hasClass('table__row--completed'),
+          mats = ele.find('.item__req[data-item="' + frag + '"]');
+
+      if(rowComplete){
+
+      } else {
+        sum += Number( mats.text() );
+      }
+    });
+    
+    var finalVal = sum.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+
+    ele.children('.event__item--total').html(finalVal);
+  })
+}
+
+function zeroItems(){
+  $.each($('.event__item'), function(){
+    var ele = $(this);
+
+    if(Number(ele.find('.event__item--total').text()) === 0) {
+      ele.addClass('event__item--zero');
+    } else {
+      ele.removeClass('event__item--zero');
+    }
+  });
+};
+
+function finishedItems(){
+  $.each($('.table__row'), function(){
+    var ele = $(this),
+        check = ele.find('.input__check--box'),
+        checkID = check.attr('ID'),
+        itemGot = false;
+
+    if (ele.find('.input__check--box:checked').length){
+      ele.addClass('table__row--completed');
+    }
+
+    check.on('click', function(){
+      ele.toggleClass('table__row--completed');
+      itemTotals();
+      zeroItems();
+
+      if(ele.hasClass('table__row--completed')){
+        itemGot = true;
+      } else {
+        itemGot = false;
+      }
+      storageUpd(checkID, itemGot);
+    })
+  });
+}
+
+
+function storageUpd(itemID, got){
+  var storageObj = {};
+
+  storageObj = JSON.parse(localStorage.getItem('aaCivilWar'));
+  storageObj[itemID] = {
+    status: got
+  }
+  localStorage.setItem('aaCivilWar', JSON.stringify(storageObj));
+}
